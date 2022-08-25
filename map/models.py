2782971bd461e87476be11195ca0b7e9b971319f
leszczynka <karolina.leszczynska05@gmail.com>
2022-08-25 10:58:56 +0200

    Initializing project traveldiary

---
+++ map/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.gis.db.models import PointField
+from django.db.models import CharField, TextField, DateField
+
+
+class Location(models.Model):
+    name = CharField(max_length=100)
+    location = PointField()
+    description = TextField()
+    date = DateField(null=True)
