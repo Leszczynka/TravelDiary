17dbddd45448bcf9dc91e7d066b844b91b8a01f9
leszczynka <karolina.leszczynska05@gmail.com>
2022-09-01 12:31:14 +0200

    Create model Marker

--- map/models.py
+++ map/models.py
@@ -3,7 +3,7 @@ from django.contrib.gis.db.models import PointField
 from django.db.models import CharField, TextField, DateField
 
 
-class Location(models.Model):
+class Marker(models.Model):
     name = CharField(max_length=100)
     location = PointField()
     description = TextField()
