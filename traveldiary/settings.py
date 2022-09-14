b139c917d75be88a00289f77df663dca115eaa9c
leszczynka <karolina.leszczynska05@gmail.com>
2022-09-14 19:01:12 +0200

    Delete google api key from settings

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -120,8 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-GOOGLE_API_KEY = 'AIzaSyDZTTewJYDP_pOk36RRaw1wHEz4Mdm62Ps'
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
