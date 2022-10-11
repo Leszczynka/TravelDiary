5f189383752eacd7add1c369af7e0c89b6bc4e15
leszczynka <karolina.leszczynska05@gmail.com>
2022-10-11 18:32:30 +0200

    move static dir to map

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'map/static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'map/static/'),
+# )
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
