4434a4089047c3ae3d5c927fc92809462582c41b
leszczynka <karolina.leszczynska05@gmail.com>
2022-10-11 19:26:23 +0200

    configure settings.py to fix bug with gdal

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -13,6 +13,12 @@ import os
 from pathlib import Path
 from django.contrib import messages
 
+
+if os.name == 'nt':
+    VENV_BASE = os.environ['VIRTUAL_ENV']
+    os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
+
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
         messages.INFO: 'alert-info',
