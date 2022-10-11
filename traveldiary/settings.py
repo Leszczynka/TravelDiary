0c4a59615fa956857836172c72cf7d4a02d8028f
leszczynka <karolina.leszczynska05@gmail.com>
2022-10-11 18:59:24 +0200

    Add Procfile and runtime.txt

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -132,9 +132,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'map/static')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'map/static/'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'map/templates'),
 )
 
 LOGIN_REDIRECT_URL = '/'
