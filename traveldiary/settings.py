48b231423ec27889d89cfc70aa9816eb6e7f9d96
leszczynka <karolina.leszczynska05@gmail.com>
2022-10-11 19:51:43 +0200

    edit settings - delete static_dirs

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -139,10 +139,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'map/static')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'map/static/'),
-)
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'map/static/'),
+# )
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'map/templates'),
