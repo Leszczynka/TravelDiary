2c88dbcbfe52a90a82884b3937fd2381ee6acc9c
leszczynka <karolina.leszczynska05@gmail.com>
2022-10-12 09:47:22 +0200

    Configure settings.py

--- traveldiary/settings.py
+++ traveldiary/settings.py
@@ -105,8 +105,9 @@ WSGI_APPLICATION = 'traveldiary.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config()
 }
-
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
